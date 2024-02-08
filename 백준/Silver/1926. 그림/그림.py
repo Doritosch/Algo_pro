232cc7f614c56b92f4efbf2f40e6bc655f2aa349
Doritosch <als000825@naver.com>
2024-02-08 23:33:44 +0900

    [Silver I] Title: 그림, Time: 320 ms, Memory: 34972 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.py
+++ 백준/Silver/1926. 그림/그림.py
@@ -1,32 +1,35 @@
 import sys
+from collections import deque
 sys.setrecursionlimit(10**6)
 
 n, m = map(int, input().split())
 paper = [list(map(int, input().split())) for _ in range(n)]
-area = 0
 answer = []
 
 mx = [1, 0, -1, 0]
 my = [0, 1, 0, -1]
 
-def dfs(x, y):
-    global area
+def bfs(x, y):
+    queue = deque()
+    queue.append((x, y))
+    paper[x][y] = 0
+    area = 1
     # 인접구역 확인
-    if paper[x][y] == 1:
-        paper[x][y] = 0
-        area += 1
+    while queue:
+        x, y = queue.popleft()
         for i in range(4):
             dx = x + mx[i]
             dy = y + my[i]
-            if (dx >= 0 and dx < n) and (dy >= 0 and dy < m):
-                dfs(dx, dy)
+            if (dx >= 0 and dx < n) and (dy >= 0 and dy < m) and paper[dx][dy] == 1:
+                paper[dx][dy] = 0
+                queue.append((dx, dy))
+                area += 1
+    return area
 
 for i in range(n):
     for j in range(m):
         if paper[i][j] == 1: 
-            dfs(i,j)
-            answer.append(area)
-            area = 0
+            answer.append(bfs(i, j))
 
 print(len(answer))
 if answer:

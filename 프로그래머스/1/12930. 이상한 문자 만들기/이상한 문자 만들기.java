266a076b68ccbce31119a3eed8a8fcd5f2fdde43
Doritosch <als000825@naver.com>
2024-07-20 15:28:33 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 14.45 ms, Memory: 88 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        int count = 0;
+        
+        for(int i=0; i<s.length(); i++) {
+            if(s.charAt(i) == ' ') {
+                answer = answer + s.charAt(i);
+                count = 0;
+                continue;
+            }
+            
+            if(count%2 == 0) {
+                answer = answer + Character.toUpperCase(s.charAt(i));
+            } else {
+                answer = answer + Character.toLowerCase(s.charAt(i));
+            }
+            count++;
+        }
+        return answer;
+    }
+}

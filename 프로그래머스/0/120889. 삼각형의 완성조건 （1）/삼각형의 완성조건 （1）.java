ac6ce59aa0f44e00940d80649fbbc31b85e36ec8
Doritosch <als000825@naver.com>
2024-08-13 15:01:04 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.60 ms, Memory: 77.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] sides) {
+        int answer = 0;
+        
+        Arrays.sort(sides);
+        
+        
+        return sides[2]>=sides[0]+sides[1]?2:1;
+    }
+}

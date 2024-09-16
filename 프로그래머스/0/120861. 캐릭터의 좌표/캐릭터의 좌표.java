79d0c6d58658be1ea2a120959bd1dc388276020a
Doritosch <als000825@naver.com>
2024-09-16 20:23:39 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.04 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] solution(String[] keyinput, int[] board) {
+        int[] answer = {0,0};
+        
+        for(String direction : keyinput) {
+            if(direction.equals("left")) {
+                if(answer[0] > board[0]/2*-1) {
+                    answer[0] -= 1;
+                }
+            }
+            else if(direction.equals("right")) {
+                if(answer[0] < board[0]/2) {
+                    answer[0] += 1;
+                }
+            }
+            else if(direction.equals("up")) {
+                if(answer[1] < board[1]/2) {
+                    answer[1] += 1;
+                }
+            }
+            else if(direction.equals("down")) {
+                if(answer[1] > board[1]/2*-1) {
+                    answer[1] -= 1;
+                }
+            }
+        }
+        return answer;
+    }
+}

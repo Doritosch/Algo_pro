bd3f4bbb049a05ef1485b7c7ef1c972adb793c60
Doritosch <als000825@naver.com>
2025-01-24 17:45:06 +0900

    [Silver V] Title: 올림픽, Time: 120 ms, Memory: 14688 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.java
+++ 백준/Silver/8979. 올림픽/올림픽.java
@@ -1,59 +1,69 @@
-import java.awt.*;
 import java.io.*;
 import java.util.*;
 
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         StringTokenizer st;
 
         st = new StringTokenizer(br.readLine());
-
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
         int[][] nations = new int[N][4];
 
-        for(int i=0; i<N; i++) {
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            int num = Integer.parseInt(st.nextToken());
-            int gold = Integer.parseInt(st.nextToken());
-            int silver = Integer.parseInt(st.nextToken());
-            int bronze = Integer.parseInt(st.nextToken());
-
-            nations[i][0] = num;
-            nations[i][1] = gold;
-            nations[i][2] = silver;
-            nations[i][3] = bronze;
+            nations[i][0] = Integer.parseInt(st.nextToken()); // 국가 번호
+            nations[i][1] = Integer.parseInt(st.nextToken()); // 금메달
+            nations[i][2] = Integer.parseInt(st.nextToken()); // 은메달
+            nations[i][3] = Integer.parseInt(st.nextToken()); // 동메달
         }
 
-        sb.append(solve(nations, K));
-        System.out.print(sb.toString());
+        System.out.println(solve(nations, K));
     }
-    private static int solve(int[][] nations, int K) {
-        Arrays.sort(nations, (o1, o2) -> {
-            if(o1[1]==o2[1]) {
-                if(o1[2]==o2[2]) {
-                    return o2[3]-o1[3];
-                }
-                return o2[2]-o1[2];
-            }
-            return o2[1]-o1[1];
-        });
 
-        int[] tempScore = {0, 0, 0};
-        int rank = 0;
-        for(int[] score:nations) {
-            if(tempScore[0]!=score[1]||tempScore[1]!=score[2]||tempScore[2]!=score[3]) {
-                tempScore[0] = score[1];
-                tempScore[1] = score[2];
-                tempScore[2] = score[3];
-                rank++;
+    private static int solve(int[][] nations, int K) {
+        // 모든 국가의 은메달, 동메달이 0인 경우
+        boolean onlyGold = true;
+        for (int[] nation : nations) {
+            if (nation[2] != 0 || nation[3] != 0) {
+                onlyGold = false;
+                break;
             }
-            if(score[0]==K) {
+        }
+
+        // 금메달만 비교하는 경우 최적화
+        if (onlyGold) {
+            Arrays.sort(nations, (o1, o2) -> o2[1] - o1[1]);
+        } else {
+            Arrays.sort(nations, (o1, o2) -> {
+                if (o1[1] == o2[1]) {
+                    if (o1[2] == o2[2]) {
+                        return o2[3] - o1[3];
+                    }
+                    return o2[2] - o1[2];
+                }
+                return o2[1] - o1[1];
+            });
+        }
+
+        int rank = 1; // 현재 순위
+        for (int i = 0; i < nations.length; i++) {
+            // 이전 국가와 점수가 동일하면 순위 유지
+            if (i > 0 && nations[i][1] == nations[i - 1][1] &&
+                nations[i][2] == nations[i - 1][2] &&
+                nations[i][3] == nations[i - 1][3]) {
+                // 동일 순위 유지
+            } else {
+                rank = i + 1; // 새로운 순위
+            }
+
+            // K번 국가 순위를 반환
+            if (nations[i][0] == K) {
                 return rank;
             }
         }
-        return rank;
+
+        return -1; // K번 국가가 없는 경우
     }
 }

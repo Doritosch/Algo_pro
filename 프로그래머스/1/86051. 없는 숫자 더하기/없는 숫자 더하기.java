be2a67e902fbc6356200153ee8572045e0924120
Doritosch <als000825@naver.com>
2025-02-27 14:28:02 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.04 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        boolean[] checked = new boolean[10];
+        
+        for(int number: numbers) {
+            checked[number] = true;
+        }
+        
+        for(int i=0; i<checked.length; i++) {
+            if(!checked[i]) {
+                answer += i;
+            }
+        }
+        return answer;
+    }
+}

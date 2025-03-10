8c8cedc4b79ff787a591b6d8bb3cf86033cbb5af
Doritosch <als000825@naver.com>
2025-03-10 19:02:50 +0900

    [level 1] Title: 신규 아이디 추천, Time: 15.81 ms, Memory: 87.9 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -1,31 +1,72 @@
 class Solution {
     public String solution(String new_id) {
-        String answer = new_id;
         
-        // 1단계
-        answer = answer.toLowerCase();
-        // 2단계
-        answer = answer.replaceAll("[^a-z0-9\\-\\_\\.]", "");
-        // 3단계
-        answer = answer.replaceAll("\\.{2,}", ".");
-        // 4단계
-        answer = answer.replaceAll("^\\.", "");
-        answer = answer.replaceAll("\\.$", "");
-        // 5단계
-        if ( answer.length() == 0 ) {
-            answer += "a";
-        }
-        // 6단계
-        if ( answer.length() >= 16) {
-            answer = answer.substring(0, 15);
-            answer = answer.replaceAll("\\.$", "");
-        }
-        // 7단계
-        if ( answer.length() <= 2 ) {
-            while(answer.length() < 3) {
-                answer += answer.charAt(answer.length()-1);
-            }
-        }
+        String answer = new KAKAOID(new_id).
+                            toLower().
+                            filter().
+                            replaceDot().
+                            removeDot().
+                            ifEmpty().
+                            substringString().
+                            concatString().
+                            getString();
+        
         return answer;
+    }
+    
+    private static class KAKAOID {
+        private String id;
+        
+        KAKAOID(String id) {
+            this.id = id;
+        }
+        
+        private KAKAOID toLower() {
+            id = id.toLowerCase();
+            return this;
+        }
+        
+        private KAKAOID filter() {
+            id = id.replaceAll("[^a-z0-9-_.]", "");
+            return this;
+        }
+        
+        private KAKAOID replaceDot() {
+            id = id.replaceAll("[.]{2,}", ".");
+            return this;
+        }
+        
+        private KAKAOID removeDot() {
+            id = id.replaceAll("^[.]|[.]$", "");
+            return this;
+        }
+        
+        private KAKAOID ifEmpty() {
+            if ( id.isEmpty() ) {
+                id = "a";
+            }
+            return this;
+        }
+        
+        private KAKAOID substringString() {
+            if ( id.length() >= 16 ) {
+                id = id.substring(0, 15);
+                id = id.replaceAll("[.]$", "");
+            }
+            return this;
+        }
+        
+        private KAKAOID concatString() {
+            if ( id.length() <= 2 ) {
+                while(id.length()<3) {
+                    id += id.charAt(id.length()-1);
+                }
+            }
+            return this;
+        }
+        
+        private String getString() {
+            return id;
+        }
     }
 }

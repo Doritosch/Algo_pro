0e6314aa8a08db26d0070829a3de62f8d37356f7
Doritosch <als000825@naver.com>
2025-03-12 20:32:33 +0900

    [Bronze II] Title: 숫자, Time: 104 ms, Memory: 14092 KB, Score: 30 point -BaekjoonHub

---
+++ 백준/Bronze/10093. 숫자/숫자.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+import java.io.*;
+import java.util.*;
+
+class Main
+{
+    public static void main(String args[]) throws Exception
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        long n = Integer.parseInt(st.nextToken());
+        long m = Integer.parseInt(st.nextToken());
+
+        long a = Math.min(n, m);
+        long b = Math.max(n, m);
+
+        if ( a==b ) {
+            sb.append(b-a).append("\n");
+        } else {
+            sb.append(b-a-1).append("\n");
+        }
+        for(long i=a+1; i<b; i++) {
+            sb.append(i).append(" ");
+        }
+        System.out.println(sb.toString());
+    }
+}

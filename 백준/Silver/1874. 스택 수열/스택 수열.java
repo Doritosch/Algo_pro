bd043393b176222df0aff8785eb917544d4521b6
Doritosch <als000825@naver.com>
2025-03-14 13:10:42 +0900

    [Silver II] Title: 스택 수열, Time: 292 ms, Memory: 25424 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,48 +1,72 @@
-import java.awt.*;
-import java.awt.desktop.SystemEventListener;
+import java.util.Scanner;
+import java.io.FileInputStream;
 import java.io.*;
 import java.util.*;
-import java.util.List;
 
-class Main {
-    public static void main(String[] args) throws IOException {
+class Main
+{
+    public static void main(String args[]) throws Exception
+    {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st;
 
-        int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
+        int n = Integer.parseInt(br.readLine());
+        Stack stack = new Stack(n);
+        int[] data = new int[n];
 
-        for(int i=0; i<N; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
+        int count = 1;
+        for(int i=0; i<n; i++) {
+            data[i] = Integer.parseInt(br.readLine());
+
+            while (data[i] != stack.peek() ) {
+                if ( count > n ) {
+                    System.out.println("NO");
+                    return;
+                }
+                sb.append(stack.push(count++)).append("\n");
+            }
+
+            sb.append(stack.pop()).append("\n");
         }
 
-        sb.append(solve(arr));
-        System.out.print(sb.toString());
+        System.out.println(sb.toString());
     }
-    private static String solve(int[] arr) {
-        StringBuilder sb = new StringBuilder();
-        Stack<Integer> stack = new Stack<>();
-        int num = 1;
 
-        for(int i=0; i<arr.length; i++) {
-            if(stack.empty()) {
-                stack.push(num++);
-                sb.append("+").append("\n");
-            }
-            while(stack.peek()!=arr[i]) {
-                if(stack.peek() < arr[i]) {
-                    stack.push(num++);
-                    sb.append("+").append("\n");
-                }
-                else if(stack.peek() > arr[i]) {
-                    return "NO";
-                }
-            }
-            stack.pop();
-            sb.append("-").append("\n");
-            }
+    static class Stack {
+        private int[] dat;
+        private int pos;
 
-        return sb.toString();
+        public Stack(int n) {
+            dat = new int[n];
+            pos = -1;
+        }
+
+        public String push(int n) {
+            dat[++pos] = n;
+            return "+";
+        }
+
+        public String pop() {
+            if ( isEmpty() ) {
+                return "";
+            }
+            pos--;
+            return "-";
+        }
+
+        public int peek() {
+            if ( isEmpty() ) {
+                return -1;
+            }
+            return dat[pos];
+        }
+        public boolean isEmpty() {
+            if ( pos == -1 ) {
+                return true;
+            } else {
+                return false;
+            }
+        }
     }
 }

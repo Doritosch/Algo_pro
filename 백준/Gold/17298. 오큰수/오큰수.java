fbbda94d004ca4af2dd530d502b2b650278e4285
Doritosch <als000825@naver.com>
2025-03-15 14:38:14 +0900

    [Gold IV] Title: 오큰수, Time: 876 ms, Memory: 156592 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -1,37 +1,49 @@
-import java.awt.*;
-import java.awt.desktop.SystemEventListener;
+import java.util.Scanner;
+import java.io.FileInputStream;
 import java.io.*;
-import java.lang.reflect.Array;
 import java.util.*;
-import java.util.List;
 
-class Main {
-    public static void main(String[] args) throws IOException {
+class Main
+{
+    public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st;
 
         int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
-        int[] answer = new int[n];
-        Arrays.fill(answer, -1);
-        Stack<Integer> stack = new Stack<>();
+        int[] nums = new int[n];
+
+        Stack<Pair> stack = new Stack<>();
 
         st = new StringTokenizer(br.readLine());
         for(int i=0; i<n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
+            int num = Integer.parseInt(st.nextToken());
 
-        for(int i=0; i<n; i++) {
-            while(!stack.empty() && arr[stack.peek()] < arr[i]) {
-                answer[stack.pop()] = arr[i];
+            while( !stack.isEmpty() && stack.peek().num < num ) {
+                Pair pair = stack.pop();
+                nums[pair.index] = num;
             }
-            stack.push(i);
+            stack.push(new Pair(i, num));
         }
 
         for(int i=0; i<n; i++) {
-            sb.append(answer[i]).append(" ");
+            if ( nums[i] == 0 ) {
+                sb.append(-1).append(" ");
+            } else {
+                sb.append(nums[i]).append(" ");
+            }
         }
+
         System.out.println(sb.toString());
+    }
+
+    static class Pair {
+        private int index;
+        private int num;
+
+        public Pair(int index, int height) {
+            this.index = index;
+            this.num = height;
+        }
     }
 }

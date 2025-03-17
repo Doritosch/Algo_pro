f498c5082e2a651716144e4a3838ced4978587a0
Doritosch <als000825@naver.com>
2025-03-17 16:31:29 +0900

    [Silver IV] Title: 카드2, Time: 164 ms, Memory: 53424 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,28 +1,29 @@
-import java.awt.*;
+import java.util.Scanner;
+import java.io.FileInputStream;
 import java.io.*;
 import java.util.*;
-import java.util.List;
 
-class Main {
-    public static void main(String[] args) throws IOException {
+class Main
+{
+    /*두 사람 A와 B가 서로 볼 수 있으려면, 두 사람 사이에 A 또는 B보다 키가 큰 사람이 없어야 한다.
+
+    줄에 서 있는 사람의 키가 주어졌을 때, 서로 볼 수 있는 쌍의 수를 구하는 프로그램을 작성하시오.*/
+    public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
 
-        int N = Integer.parseInt(br.readLine());
-        Queue<Integer> card = new LinkedList<>();
-
-        for(int i=1; i<=N; i++) {
-            card.add(i);
+        int n = Integer.parseInt(br.readLine());
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=1; i<=n; i++) {
+            q.add(i);
         }
 
-        while(card.size() != 1) {
-            card.poll();
-
-            int num = card.poll();
-            card.add(num);
+        while(q.size() != 1) {
+            q.poll();
+            int card = q.poll();
+            q.add(card);
         }
-
-        sb.append(card.poll());
-        System.out.print(sb.toString());
+        System.out.println(q.poll());
     }
 }

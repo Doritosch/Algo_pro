8843716f19f6b76e0fcb985b89aa9a850d1b59db
Doritosch <als000825@naver.com>
2025-04-07 22:37:51 +0900

    [Silver III] Title: N과 M (3), Time: 380 ms, Memory: 67792 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static int[] arr;
+    static int n,m;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[m];
+
+        backTracking(0);
+
+        System.out.println(sb.toString());
+    }
+    public static void backTracking(int depth) {
+        if ( depth == m ) {
+            for(int i=0; i<m; i++) {
+                sb.append(arr[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i=1; i<=n; i++) {
+            arr[depth] = i;
+            backTracking(depth+1);
+        }
+    }
+}

40bdf30ce3170a26106efe0436990e755d88ef68
Doritosch <als000825@naver.com>
2025-07-08 21:34:26 +0900

    [Gold IV] Title: 팰린드롬?, Time: 752 ms, Memory: 219072 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
@@ -10,10 +10,29 @@ class Main
 
         int n = Integer.parseInt(br.readLine());
         int[] nums = new int[n+1];
+        boolean[][] dp = new boolean[n+1][n+1];
 
         st = new StringTokenizer(br.readLine());
         for(int i=1; i<=n; i++) {
             nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i=1; i<=n; i++) {
+            dp[i][i] = true;
+        }
+
+        for(int i=1; i<=n-1; i++) {
+            if ( nums[i] == nums[i+1] ) {
+                dp[i][i+1] = true;
+            }
+        }
+
+        for(int i=2; i<n; i++) {
+            for(int j=1; j<=n-i; j++) {
+                if ( nums[j] == nums[j+i] && dp[j+1][j+i-1] ) {
+                    dp[j][j+i] = true;
+                }
+            }
         }
 
         int m = Integer.parseInt(br.readLine());
@@ -22,15 +41,7 @@ class Main
             int s = Integer.parseInt(st.nextToken());
             int e = Integer.parseInt(st.nextToken());
 
-            boolean flag = true;
-
-            while(s<e) {
-                if ( nums[s++] != nums[e--] ) {
-                    flag = false;
-                    break;
-                }
-            }
-            if ( flag ) {
+            if ( dp[s][e] ) {
                 sb.append(1).append("\n");
             } else {
                 sb.append(0).append("\n");

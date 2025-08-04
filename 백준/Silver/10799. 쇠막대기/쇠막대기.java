eae0733b785bbdba1e7bd4a19f3c215e39574705
Doritosch <als000825@naver.com>
2025-08-04 23:47:08 +0900

    [Silver II] Title: 쇠막대기, Time: 168 ms, Memory: 16344 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.java
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -1,36 +1,30 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class Main {
-
-    public static void main(String[] args) throws Exception {
+class Main {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        String line = br.readLine();
+
+        Stack<Character> stack = new Stack<>();
         int result = 0;
 
-        String input = br.readLine();
-        Stack<Character> stack = new Stack<>();
+        for(int i=0; i<line.length(); i++) {
+            char c = line.charAt(i);
 
-        for(int i=0; i<input.length(); i++) {
-            char ch = input.charAt(i);
-
-            if ( ch == '(' ) {
-                stack.push(ch);
-                continue;
-            }
-
-            if ( ch == ')' ) {
+            if (c == '(') {
+                stack.push(c);
+            } else {
                 stack.pop();
 
-                if ( input.charAt(i-1) == '(' ) {
-                    result += stack.size();
+                if (line.charAt(i-1) == ')') {
+                    result += 1;
                 } else {
-                    result++;
+                    result += stack.size();
                 }
             }
         }
-
         System.out.println(result);
     }
-
 }

5eb734e3cc5a95b9a642b9b8fa3a65c59eb6cbf1
Doritosch <als000825@naver.com>
2025-09-16 10:31:14 +0900

    [level 1] Title: 약수의 합, Time: 0.09 ms, Memory: 90.8 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.java
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -1,11 +1,11 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0;
+        int sum = 0;
         for(int i=1; i<=n; i++) {
-            if(n%i==0) {
-                answer += i;
+            if (n%i == 0) {
+                sum += i;
             }
         }
-        return answer;
+        return sum;
     }
 }

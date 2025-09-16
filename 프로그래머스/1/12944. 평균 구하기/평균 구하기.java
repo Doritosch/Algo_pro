082085f954285afde85da8ad2504425c05c0bd13
Doritosch <als000825@naver.com>
2025-09-16 10:27:35 +0900

    [level 1] Title: 평균 구하기, Time: 0.05 ms, Memory: 84 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -1,11 +1,11 @@
 class Solution {
     public double solution(int[] arr) {
-        double answer = 0;
+        double sum = 0;
+        double average = 0;
         for(int i=0; i<arr.length; i++) {
-            answer += arr[i];
+            sum += (double)arr[i];
         }
-        
-        answer = answer/arr.length;
-        return answer;
+        average = sum / arr.length;
+        return average;
     }
 }

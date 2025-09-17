55d6462b46a32c58784b7632480f15bc5feed7c8
Doritosch <als000825@naver.com>
2025-09-17 10:30:19 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.06 ms, Memory: 83 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,17 +1,20 @@
 class Solution {
     public int solution(int num) {
-        int answer = 0;
-        long value = num;
-        while(answer<=500) {
-            if(value==1) {
-                return answer;
+        int i = 0;
+        long n = (long)num;
+        // 입력된 수가 짝수라면 2로 나눈다.
+        // 입력된 수가 홀수라면 3을 곱하고 1을 더한다.
+        while(i < 500) {
+            if (n == 1) {
+                return i;
             }
-            if(value%2==0) {
-                value/=2;
+            
+            if (n%2 == 0) {
+                n /= 2;
             } else {
-                value = (value*3) + 1;
+                n = n*3 + 1;
             }
-            answer++;
+            i++;
         }
         return -1;
     }

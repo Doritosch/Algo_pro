eaf7e624e58412cdeafab00af91e625d473c694b
Doritosch <als000825@naver.com>
2025-09-19 10:07:47 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.02 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -1,29 +1,14 @@
 class Solution {
-    private int[] getAnswer(int n, int m) {
-        int gcd = 0;
-        int lcm = 0;
-        for(int i=m; i>0; i--) {
-            if(m%i==0 && n%i==0) {
-                gcd = i;
-                break;
-            }
-        }
-        for(int i=1; i<=n; i++) {
-            if((n*i)%m==0) {
-                lcm = n*i;
-                break;
-            }
-        }
-        return new int[] {gcd, lcm};
+    public int[] solution(int n, int m) {
+        return new int[]{getGCD(n,m), getLCM(n,m)};
     }
-    public int[] solution(int n, int m) { 
-        int[] answer;
-        if(n>m) {
-            answer = getAnswer(n,m);
-        } else {
-            answer = getAnswer(m,n);
+    public int getGCD(int n, int m) {
+        if (m == 0) {
+            return n;
         }
-        
-        return answer;
+        return getGCD(m, n%m);
+    }
+    public int getLCM(int n, int m) {
+        return (n*m) / getGCD(n,m);
     }
 }

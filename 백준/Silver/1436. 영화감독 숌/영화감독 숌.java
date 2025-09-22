6aafd820961a99323f5e21f250ce1741a862e8f9
Doritosch <als000825@naver.com>
2025-09-22 10:47:25 +0900

    [Silver V] Title: 영화감독 숌, Time: 280 ms, Memory: 85804 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int count = 1;
+        int num = 666;
+        while(count != n) {
+            num++;
+            if (String.valueOf(num).contains("666")) {
+                count++;
+            }
+        }
+        System.out.println(num);
+    }
+}

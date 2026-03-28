076418db7a16b19446b1ee9a43de9f36b69231be
Doritosch <als000825@naver.com>
2026-03-28 14:12:57 +0900

    [level 2] Title: 올바른 괄호, Time: 16.88 ms, Memory: 54.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        Stack<String> stack = new Stack<>();
+        
+        for(int i=0; i<s.length(); i++) {
+            char c = s.charAt(i);
+            
+            if (c == '(') {
+                stack.add("(");
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+        
+        if (!stack.isEmpty()) {
+            return false;
+        }
+        return true;
+    }
+}

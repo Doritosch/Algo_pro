f0fd3427ded8a4fcd06594e2d878e1e9775d525d
Doritosch <als000825@naver.com>
2026-05-02 18:29:31 +0900

    [level 1] Title: K번째수, Time: 2.18 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,20 +1,27 @@
 import java.util.*;
+
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        int[] answer = new int[commands.length];
+        List<Integer> list = new ArrayList<>();
         
         for(int i=0; i<commands.length; i++) {
-            int length = commands[i][1]-commands[i][0]+1;
-            int[] copyArr = new int[length];
+            int len = commands[i][1] - commands[i][0] + 1;
+            int[] arr = new int[len];
             
-            for(int j=0; j<length; j++) {
-                copyArr[j] = array[commands[i][0]+j-1];
+            for(int j=0; j<len; j++) {
+                arr[j] = array[commands[i][0]-1+j];
             }
-            
-            Arrays.sort(copyArr);
-            
-            answer[i] = copyArr[commands[i][2]-1];
+            for(int j=0; j<len-1; j++) {
+                for(int k=j+1; k<len; k++) {
+                    if (arr[j] > arr[k]) {
+                        int temp = arr[j];
+                        arr[j] = arr[k];
+                        arr[k] = temp;
+                    }
+                }
+            }
+            list.add(arr[commands[i][2]-1]);
         }
-        return answer;
+        return list.stream().mapToInt(i->i).toArray();
     }
 }

98966fd3b74bf2b1640d9eb28428cfb02135aea6
Doritosch <als000825@naver.com>
2026-05-02 18:37:46 +0900

    [level 1] Title: K번째수, Time: 7.18 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -5,22 +5,14 @@ class Solution {
         List<Integer> list = new ArrayList<>();
         
         for(int i=0; i<commands.length; i++) {
-            int len = commands[i][1] - commands[i][0] + 1;
-            int[] arr = new int[len];
+            int start = commands[i][0]-1;
+            int end = commands[i][1];
+            int target = commands[i][2];
             
-            for(int j=0; j<len; j++) {
-                arr[j] = array[commands[i][0]-1+j];
-            }
-            for(int j=0; j<len-1; j++) {
-                for(int k=j+1; k<len; k++) {
-                    if (arr[j] > arr[k]) {
-                        int temp = arr[j];
-                        arr[j] = arr[k];
-                        arr[k] = temp;
-                    }
-                }
-            }
-            list.add(arr[commands[i][2]-1]);
+            int[] arr = Arrays.copyOfRange(array, start, end);
+            Arrays.sort(arr);
+            
+            list.add(arr[target-1]);
         }
         return list.stream().mapToInt(i->i).toArray();
     }

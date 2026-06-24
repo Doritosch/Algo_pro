a4b210f1b64018e6b107ea47fcb6ef3ec15b800b
Doritosch <als000825@naver.com>
2026-06-24 15:01:48 +0900

    [Passed] 연속되는 수 4 (140ms, 10MB)

---
+++ trail2/연속되는 수 4/continuous-number4.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i=0; i<n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int cnt = 0;
+        int max = 0;
+        for(int i=0; i<n; i++) {
+            if (i==0 || arr[i] > arr[i-1]) {
+                cnt += 1;
+            } else {
+                cnt = 1;
+            }
+            max = Math.max(max, cnt);
+        }
+
+        System.out.print(max);
+    }
+}

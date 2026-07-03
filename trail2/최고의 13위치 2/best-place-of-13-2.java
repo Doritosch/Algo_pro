8e93810078399baa53cffce0a8c3a3a191ee88c2
Doritosch <als000825@naver.com>
2026-07-03 11:49:34 +0900

    [Passed] 최고의 13위치 2 (135ms, 10MB)

--- trail2/최고의 13위치 2/best-place-of-13-2.java
+++ trail2/최고의 13위치 2/best-place-of-13-2.java
@@ -1,35 +1,30 @@
-import java.util.*;
-
+import java.util.Scanner;
 public class Main {
-    private static int n;
-    private static final int INT_MIN = Integer.MIN_VALUE;
+    public static final int INT_MIN = Integer.MIN_VALUE;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        n = sc.nextInt();
-        int[][] grid = new int[n][n];
-
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<n; j++) {
-                grid[i][j] = sc.nextInt();
-            }
-        }
-
+        int n = sc.nextInt();
+        int[][] arr = new int[n][n];
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                arr[i][j] = sc.nextInt();
+        // Please write your code here.
         int max = INT_MIN;
         for(int i=0; i<n; i++) {
-            for(int j=0; j<=n-3; j++) {
+            for(int j=0; j<n-2; j++) {
                 for(int k=i; k<n; k++) {
-                    for(int l=0; l<=n-3; l++) {
-                        if (i==k && !(j+3 <= l || l+3 <= j)) {
+                    for(int l=j; l<n-2; l++) {
+                        if (i==k && (Math.abs(j-l) <= 2)) {
                             continue;
                         }
-                        max = Math.max(max, grid[i][j] + grid[i][j+1] + grid[i][j+2]
-                                    + grid[k][l] + grid[k][l+1] + grid[k][l+2]);
+
+                        max = Math.max(max, arr[i][j] + arr[i][j+1] + arr[i][j+2]
+                                        + arr[k][l] + arr[k][l+1] + arr[k][l+2]);
                     }
                 }
             }
         }
-
+        
         System.out.print(max);
     }
 }

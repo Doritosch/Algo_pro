a735e8f5451c828fa56eba6da46a7abaa460e8a3
Doritosch <als000825@naver.com>
2026-07-24 00:00:27 +0900

    [Passed] 두 점으로 만드는 정사각형 (131ms, 10MB)

---
+++ trail2/두 점으로 만드는 정사각형/square-made-of-two-points.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x1 = sc.nextInt();
+        int y1 = sc.nextInt();
+        int x2 = sc.nextInt();
+        int y2 = sc.nextInt();
+        int a1 = sc.nextInt();
+        int b1 = sc.nextInt();
+        int a2 = sc.nextInt();
+        int b2 = sc.nextInt();
+        // Please write your code here.
+        int r1 = Math.min(x1, a1);
+        int s1 = Math.min(y1, b1);
+        int r2 = Math.max(x2, a2);
+        int s2 = Math.max(y2, b2);
+        int result = Math.max(r2 - r1, s2 - s1);
+        result *= result;
+        System.out.print(result);
+    }
+}

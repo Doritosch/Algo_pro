a14b6da79d5de648fd10e4ba6eafea1ed15b293c
Doritosch <als000825@naver.com>
2026-07-23 23:54:54 +0900

    [Passed] A, B, C, D 찾기 (132ms, 10MB)

---
+++ trail2/A, B, C, D 찾기/find-a-b-c-d.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[15];
+        for (int i = 0; i < 15; i++) {
+            arr[i] = sc.nextInt();
+        }
+        // Please write your code here.
+        Arrays.sort(arr);
+        
+        int a = arr[0];
+        int b = arr[1];
+        int c = arr[2];
+        int d = arr[arr.length - 1] - a - b - c;
+        System.out.print(a + " " + b + " " + c + " " + d);
+    }
+}

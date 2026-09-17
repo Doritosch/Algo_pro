3a279aa836453d76b6049b0cc5b30089ebf838cc
Doritosch <als000825@naver.com>
2026-09-17 15:24:57 +0900

    [level 2] Title: 주식가격, Time: 21.70 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -2,20 +2,20 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] prices) {
-        int[] answer = new int[prices.length];
         Stack<Integer> stack = new Stack<>();
+        int[] answer = new int[prices.length];
         
         for(int i=0; i<prices.length; i++) {
             while(!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
-                int peek = stack.pop();
-                answer[peek] = i - peek;
+                int idx = stack.pop();
+                answer[idx] = i - idx;
             }
             stack.push(i);
         }
         
         while(!stack.isEmpty()) {
-            int p = stack.pop();
-            answer[p] = prices.length - p - 1;
+            int idx = stack.pop();
+            answer[idx] = prices.length - idx - 1;
         }
         
         return answer;

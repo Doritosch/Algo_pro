fd754d16914fde3459d7b97627c841002c725f36
Doritosch <als000825@naver.com>
2026-09-20 14:56:08 +0900

    [level 2] Title: 더 맵게, Time: 673.19 ms, Memory: 140 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/README.md
+++ 프로그래머스/2/42626. 더 맵게/README.md
@@ -1,10 +1,10 @@
 # [level 2] 더 맵게 - 42626 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626) 
 
 ### 성능 요약
 
-메모리: 128 MB, 시간: 1825.12 ms
+메모리: 140 MB, 시간: 673.19 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 05월 02일 16:40:52
+2026년 09월 20일 14:56:05
 
 ### 문제 설명
 
--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -1,22 +1,22 @@
 import java.util.*;
+
 class Solution {
     public int solution(int[] scoville, int K) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        PriorityQueue<Integer> q = new PriorityQueue<>();
         
-        for(int s : scoville) {
-            pq.offer(s);
+        for(int i=0; i<scoville.length; i++) {
+            q.offer(scoville[i]);
         }
         
         int count = 0;
-        while(pq.peek() < K) {
-            if (pq.size() <= 1) {
-               return -1; 
+        while(q.peek() < K) {
+            int f = q.poll();
+            if (q.isEmpty()) {
+                return -1;
             }
             
-            int first = pq.poll();
-            int cal = first + (pq.poll() * 2);
-            pq.offer(cal);
-            count++;
+            q.offer(f+(q.poll()*2));
+            count += 1;
         }
         return count;
     }

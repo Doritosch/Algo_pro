0b37f1fbdc39bf076b80cb33c457f8f2f45db99a
Doritosch <als000825@naver.com>
2026-09-26 14:59:19 +0900

    [level 2] Title: 타겟 넘버, Time: 4.06 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,18 +1,21 @@
 class Solution {
-    static int answer;
+    int answer;
     public int solution(int[] numbers, int target) {
         answer = 0;
-        dfs(numbers, 0, 0, target);
+        dfs(numbers, target, 0, 0);
         return answer;
     }
-    public void dfs(int[] numbers, int idx, int sum, int target) {
-        if (idx == numbers.length) {
-            if (sum == target) {
-                answer += 1;
-            }
+    public void dfs(int[] numbers, int target, int idx, int num) {
+        if (idx >= numbers.length) {
             return;
         }
-        dfs(numbers, idx+1, sum+numbers[idx], target);
-        dfs(numbers, idx+1, sum-numbers[idx], target);
+        if (numbers.length-1 == idx && 
+            (num+numbers[idx] == target || num-numbers[idx] == target)) {
+            answer += 1;
+            return;
+        }
+        
+        dfs(numbers, target, idx+1, num+numbers[idx]);
+        dfs(numbers, target, idx+1, num-numbers[idx]);
     }
 }
